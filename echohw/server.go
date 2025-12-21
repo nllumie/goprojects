9cf4cee64aed312f70bac08b4e5946963208ce2e
nllumie <abikenova.nd@gmail.com>
2025-12-21 20:27:10 +0500

    hw 2

---
+++ echohw/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.RequestLogger()) // use the default RequestLogger middleware with slog logger
+	e.Use(middleware.Recover())       // recover panics as errors for proper error handling
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start server", "error", err)
+	}
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
